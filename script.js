ffa2c96a3e89441cc2d59411c4fc80cbeb043adf
Umashankar Ahirwar <157135198+UmashankarAhirwar20@users.noreply.github.com>
2025-11-18 15:50:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+let bookForm = document.getElementById("book-form");
+let titleInput = document.getElementById("title");
+let authorInput = document.getElementById("author");
+let isbnInput = document.getElementById("isbn");
+let bookList = document.getElementById("book-list");
+
+bookForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let title = titleInput.value.trim();
+  let author = authorInput.value.trim();
+  let isbn = isbnInput.value.trim();
+
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+      <td>${title}</td>
+      <td>${author}</td>
+      <td>${isbn}</td>
+      <td><button class="btn btn-danger btn-sm delete">Delete</button></td>
+    `;
+  bookList.appendChild(tr);
+
+  titleInput.value = "";
+  authorInput.value = "";
+  isbnInput.value = "";
+});
+
+bookList.addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete")) {
+    const row = e.target.closest("tr");
+    row.remove();
+  }
+});
